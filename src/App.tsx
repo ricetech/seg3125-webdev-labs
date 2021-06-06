487de9e3642c4e83ac6eb51121f9ccdb1aceb8ae
Eric C <39015447+ricetech@users.noreply.github.com>
2021-06-05 23:15:26 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import './App.scss';
+
+const App = () => {
+  return (
+    <Switch>
+
+    </Switch>
+  );
+}
+
+export default App;
