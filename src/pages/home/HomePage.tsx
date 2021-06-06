f66688e2ac795aefafde129baa752c3036516b22
Eric C <39015447+ricetech@users.noreply.github.com>
2021-06-06 04:18:37 -0400

    Add HomePage content

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import './_home.scss';
+import { Col, Container, Row } from "react-bootstrap";
 
 export const HomePage = () => {
     return (
-        <>
-            HomePage
-        </>
+        <Container className='mt-4'>
+            <Row>
+              <Col>
+                <h1>SEG 3125 Static Coursework - Eric Chen</h1>
+                <p>Please use the navbar to browse this website.</p>
+              </Col>
+            </Row>
+        </Container>
     );
 }
