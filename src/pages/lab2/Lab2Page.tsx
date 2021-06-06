5b8ff4f4ad7b2f3510da59addbcc9943b1c740ea
Eric C <39015447+ricetech@users.noreply.github.com>
2021-06-06 19:52:28 -0400

    Implement Lab2PreferencesPage

--- src/pages/lab2/Lab2Page.tsx
+++ src/pages/lab2/Lab2Page.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import { Col, Container, Row } from "react-bootstrap";
 
 import { Restrictions } from "./enums/restrictions";
@@ -14,8 +14,30 @@ import './_lab2.scss';
 export const Lab2Page = () => {
   let { path } = useRouteMatch();
 
-  const [preferences, setPreferences] = useState<Restrictions[]>([]);
-  const [cart, setCart] = useState<Product[]>([]);
+  const [ preferences, setPreferences ] = useState<Restrictions[]>([]);
+
+  const [ lactoseFree, setLactoseFree ] = useState(false);
+  const [ nutFree, setNutFree ] = useState(false);
+  const [ organic, setOrganic ] = useState(false);
+
+  const [ cart, setCart ] = useState<Product[]>([]);
+
+  // Keep preferences up-to-date
+  useEffect(() => {
+    const newPreferences = [];
+    if (lactoseFree) {
+      newPreferences.push(Restrictions.LactoseFree);
+    }
+    if (nutFree) {
+      newPreferences.push(Restrictions.NutFree);
+    }
+    if (organic) {
+      newPreferences.push(Restrictions.Organic);
+    }
+
+    setPreferences(newPreferences);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lactoseFree, nutFree, organic])
 
   return (
     <>
@@ -32,7 +54,9 @@ export const Lab2Page = () => {
             <Lab2ProductsPage preferences={preferences} setPreferences={setPreferences} cart={cart} setCart={setCart} />
           </Route>
           <Route path={`${path}/preferences`}>
-            <Lab2PreferencesPage preferences={preferences} setPreferences={setPreferences} />
+            <Lab2PreferencesPage preferences={preferences} setPreferences={setPreferences} lactoseFree={lactoseFree}
+                                 setLactoseFree={setLactoseFree} nutFree={nutFree} setNutFree={setNutFree}
+                                 organic={organic} setOrganic={setOrganic} />
           </Route>
           <Route path={`${path}/cart`}>
             <Lab2CartPage cart={cart} setCart={setCart} />
--- src/pages/lab2/pages/preferences/Lab2PreferencesPage.tsx
+++ src/pages/lab2/pages/preferences/Lab2PreferencesPage.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Col, Container, Row } from "react-bootstrap";
+import React from 'react';
+import { Button, Col, Form, Row } from "react-bootstrap";
 
 import { Restrictions } from "../../enums/restrictions";
 
@@ -8,12 +8,71 @@ import './_lab2-preferences.scss';
 interface Lab2PreferencesPageProps {
   preferences: Restrictions[];
   setPreferences: React.Dispatch<React.SetStateAction<Restrictions[]>>;
+  lactoseFree: boolean;
+  setLactoseFree: React.Dispatch<React.SetStateAction<boolean>>;
+  nutFree: boolean;
+  setNutFree: React.Dispatch<React.SetStateAction<boolean>>;
+  organic: boolean;
+  setOrganic: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const Lab2PreferencesPage = (props: Lab2PreferencesPageProps) => {
+  const handleChange = (restriction: Restrictions, event: React.ChangeEvent<HTMLInputElement>) => {
+    switch (restriction) {
+      case Restrictions.LactoseFree:
+        props.setLactoseFree(event.target.checked);
+        break;
+      case Restrictions.NutFree:
+        props.setNutFree(event.target.checked);
+        break;
+      case Restrictions.Organic:
+        props.setOrganic(event.target.checked);
+        break;
+    }
+  }
+
+  const handleSubmit = () => {
+    const restrictions = [];
+    if (props.lactoseFree) {
+      restrictions.push(Restrictions.LactoseFree);
+    }
+    if (props.nutFree) {
+      restrictions.push(Restrictions.NutFree);
+    }
+    if (props.organic) {
+      restrictions.push(Restrictions.Organic);
+    }
+
+    props.setPreferences(restrictions);
+  }
+
   return (
     <>
-      Preferences
+      <Row>
+        <Col>
+          <h2>Preferences</h2>
+          <p>Please set your dietary restrictions/preferences below.</p>
+          <p><b>Note that your preferences will be lost if you navigate away from the Lab 2 pages.</b></p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form>
+            <Form.Group controlId='pref'>
+              <Form.Check label='Lactose Free' checked={props.lactoseFree}
+                          onChange={(event) => handleChange(Restrictions.LactoseFree, event)} />
+              <Form.Check label='Nut Free' checked={props.nutFree}
+                          onChange={(event) => handleChange(Restrictions.NutFree, event)} />
+              <Form.Check label='Organic' checked={props.organic}
+                          onChange={(event) => handleChange(Restrictions.Organic, event)} />
+            </Form.Group>
+            <Button type='submit' onClick={(event) => {
+              event.preventDefault();
+              handleSubmit();
+            }}>Save</Button>
+          </Form>
+        </Col>
+      </Row>
     </>
   );
 }
