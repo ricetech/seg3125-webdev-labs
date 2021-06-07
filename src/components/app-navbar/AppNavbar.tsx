c33f7774d691c4543b8f74044b2081f74bc74642
Eric C <39015447+ricetech@users.noreply.github.com>
2021-06-06 20:04:45 -0400

    Fix nested anchor tag warning

--- src/components/app-navbar/AppNavbar.tsx
+++ src/components/app-navbar/AppNavbar.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import './_app-navbar.scss';
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 export const AppNavbar = () => {
+  let location = useLocation();
+
   return (
     <Navbar variant='dark' expand='md' sticky='top' className='app-navbar'>
       <Navbar.Brand as={Link} to='/' className='mr-2'>
@@ -15,7 +17,7 @@ export const AppNavbar = () => {
         <Nav className='mr-auto'>
           <Nav.Link as={NavLink} exact to='/' activeClassName='active'>Home</Nav.Link>
           <Nav.Link as={NavLink} to='/lab1' activeClassName='active'>Lab 1</Nav.Link>
-          <NavDropdown id='lab2-dropdown' title='Lab 2' to='/lab2' as={NavLink} className='no-underline-link' activeClassName='active'>
+          <NavDropdown id='lab2-dropdown' title='Lab 2' className={location.pathname.includes('/lab2') ? 'active' : ''}>
             <NavDropdown.Item as={NavLink} to='/lab2/products' activeClassName='active'>Products</NavDropdown.Item>
             <NavDropdown.Item as={NavLink} to='/lab2/preferences' activeClassName='active'>
               Preferences
