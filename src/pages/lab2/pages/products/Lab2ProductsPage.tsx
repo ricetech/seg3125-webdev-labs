dd0e9ba1e328b76008d51cb8f1d1f294d9fe8b1f
Eric C <39015447+ricetech@users.noreply.github.com>
2021-06-06 20:25:07 -0400

    Add products table to Lab2ProductsPage.tsx

--- src/pages/lab2/pages/products/Lab2ProductsPage.tsx
+++ src/pages/lab2/pages/products/Lab2ProductsPage.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import { Restrictions } from "../../enums/restrictions";
 import { Product } from "../../interfaces/product";
 
 import './_lab2-products.scss';
+import { products } from "../../data/products";
 
 interface Lab2ProductsPageProps {
   preferences: Restrictions[];
@@ -16,7 +18,39 @@ interface Lab2ProductsPageProps {
 export const Lab2ProductsPage = (props: Lab2ProductsPageProps) => {
   return (
     <>
-      Products
+      <Row>
+        <Col>
+          <h2>Products</h2>
+          <p>
+            Based on your <Link to='/lab2/preferences'>preferences</Link>, we have curated the following
+            products for you.
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Table hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr>
+                  <td>{product.id}</td>
+                  <td>{product.name}</td>
+                  <td>${product.price.toFixed(2)}</td>
+                  <td><Button size='sm'>Add to Cart</Button></td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
     </>
   );
 }
