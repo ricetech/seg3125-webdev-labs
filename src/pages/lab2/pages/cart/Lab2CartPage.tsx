c921e3bc1bb3f856e229887209f5df6ffc7bd16f
Eric C <39015447+ricetech@users.noreply.github.com>
2021-06-06 21:55:56 -0400

    Implement Cart Page

--- src/pages/lab2/pages/cart/Lab2CartPage.tsx
+++ src/pages/lab2/pages/cart/Lab2CartPage.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import { Col, Container, Row } from "react-bootstrap";
+import React from 'react';
+import { Col, Row, Table } from "react-bootstrap";
 
-import { Product } from "../../interfaces/product";
+import { priceComparator } from "../../interfaces/product";
 
 import './_lab2-cart.scss';
+import { products } from "../../data/products";
 
 interface Lab2CartPageProps {
   cart: Set<number>;
@@ -11,9 +12,52 @@ interface Lab2CartPageProps {
 }
 
 export const Lab2CartPage = (props: Lab2CartPageProps) => {
+  const inCart = (itemId: number) => props.cart.has(itemId);
+
+  const sortedFilteredProducts = products.sort(priceComparator).filter((product) => (inCart(product.id)));
+
+  const cartTotal = (): string => {
+    let total = 0;
+    sortedFilteredProducts.forEach((product) => {
+      total += product.price;
+    })
+    return total.toFixed(2);
+  }
+
   return (
     <>
-      Cart
+      <Row>
+        <Col>
+          <h2>Cart</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Table hover>
+            <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+            </thead>
+            <tbody>
+            {sortedFilteredProducts.map((product) => (
+              <tr>
+                <td>{product.id}</td>
+                <td>{product.name}</td>
+                <td>${product.price.toFixed(2)}</td>
+              </tr>
+            ))}
+            <tr>
+              <td />
+              <td><b>Total</b></td>
+              <td>{`$${cartTotal()}`}</td>
+            </tr>
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
     </>
   );
 }
