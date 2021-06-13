3bd4953172d209e00aad698697c9fa83f5d6d36f
Eric C <39015447+ricetech@users.noreply.github.com>
2021-06-13 19:58:11 -0400

    Split Expert Cards into 2 rows

--- src/pages/lab4/components/Lab4ExpertCards/Lab4ExpertCards.tsx
+++ src/pages/lab4/components/Lab4ExpertCards/Lab4ExpertCards.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardDeck } from "react-bootstrap";
+import { Card, CardDeck, Col, Row } from "react-bootstrap";
 
 import "./_lab4-expert-cards.scss";
 
@@ -7,29 +7,46 @@ export const Lab4ExpertCards = () => {
   return (
     <>
       <h3>Experts</h3>
-      <CardDeck>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Person</Card.Title>
-            <Card.Text>Description</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Person</Card.Title>
-            <Card.Text>Description</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Person</Card.Title>
-            <Card.Text>Description</Card.Text>
-          </Card.Body>
-        </Card>
-      </CardDeck>
+      <Row>
+        <Col>
+          <CardDeck>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Person</Card.Title>
+                <Card.Text>Description</Card.Text>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Person</Card.Title>
+                <Card.Text>Description</Card.Text>
+              </Card.Body>
+            </Card>
+          </CardDeck>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col>
+          <CardDeck>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Person</Card.Title>
+                <Card.Text>Description</Card.Text>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Person</Card.Title>
+                <Card.Text>Description</Card.Text>
+              </Card.Body>
+            </Card>
+          </CardDeck>
+        </Col>
+      </Row>
     </>
   );
 };
