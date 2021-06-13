debdef8ab0246656a8032cc17be3c669b190d461
Eric C <39015447+ricetech@users.noreply.github.com>
2021-06-13 19:54:34 -0400

    Create Service Table structure

--- src/pages/lab4/components/Lab4ServicesTable/Lab4ServicesTable.tsx
+++ src/pages/lab4/components/Lab4ServicesTable/Lab4ServicesTable.tsx
@@ -1,11 +1,42 @@
 import React from "react";
 
 import "./_lab4-services-table.scss";
+import { Table } from "react-bootstrap";
 
 export const Lab4ServicesTable = () => {
   return (
     <>
       <h3>Services</h3>
+      <Table hover>
+        <thead>
+          <tr>
+            <th>Service</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Service 1</td>
+            <td>$10.00</td>
+          </tr>
+          <tr>
+            <td>Service 2</td>
+            <td>$10.00</td>
+          </tr>
+          <tr>
+            <td>Service 3</td>
+            <td>$10.00</td>
+          </tr>
+          <tr>
+            <td>Service 4</td>
+            <td>$10.00</td>
+          </tr>
+          <tr>
+            <td>Service 5</td>
+            <td>$10.00</td>
+          </tr>
+        </tbody>
+      </Table>
     </>
   );
 };
