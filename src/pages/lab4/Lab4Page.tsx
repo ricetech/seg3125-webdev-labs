51fd886708897c5b13a8eb85ca7cef000408d225
Eric C <39015447+ricetech@users.noreply.github.com>
2021-06-13 19:18:17 -0400

    Create basic Lab4Page structure

--- src/pages/lab4/Lab4Page.tsx
+++ src/pages/lab4/Lab4Page.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 export const Lab4Page = () => {
   return (
     <>
-      <Container className='mt-4 container__fill'>
+      <Container className="mt-4 container__fill">
         <Row>
           <h1>Lab 4 - Hannah's Physiotherapy Clinic</h1>
           <p>5575 Second Line, Erin, ON</p>
         </Row>
         <hr />
+        <Row>
+          <Col>
+            <h3>Services</h3>
+          </Col>
+          <Col>
+            <h3>Experts</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>Book an appointment</h3>
+          </Col>
+        </Row>
       </Container>
     </>
-  )
-}
+  );
+};
